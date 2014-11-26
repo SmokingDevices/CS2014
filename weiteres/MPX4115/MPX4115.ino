177284188bedb7d89ea81e73a6c6a604118b9e51
Steffen Janke <Steffen.Janke@gmail.com>
2014-11-26 10:02:07 +0100

    da lagen noch Test auf meinem Rechner, na gut, online

--- weiteres/MPX4115/MPX4115.ino
+++ weiteres/MPX4115/MPX4115.ino
@@ -1,4 +1,4 @@
-const int analogpin = 6;  // Analog input pin 
+const int analogpin = 0;  // Analog input pin 
 long letzteSendung = 0;
 
 String floatToString(float number){
@@ -13,7 +13,7 @@ float getPressure (int pin) {  //Methode für Druck
   float hPa=0;
   for (int i=1; i<=10; i++) {
     hPa += (float)(((analogRead(pin)/1024.0)+0.095)/0.0009);
-    hPa += 17.0;
+    hPa += 7.0;
   }
 //  hPa = hPa/5.0;
   return hPa/10.0;  // zurueckgeben
